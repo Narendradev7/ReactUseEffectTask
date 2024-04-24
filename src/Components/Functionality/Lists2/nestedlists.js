c97f42cae36ae2bf226c62e304a39057766ac13d
Narendradev7 <venkatanarendrak7@gmail.com>
2024-04-24 21:34:04 +0530

    Fourth class

--- src/Components/Functionality/Lists2/nestedlists.js
+++ src/Components/Functionality/Lists2/nestedlists.js
@@ -6,93 +6,121 @@
     const IPLHistory = [
 
                         {
-                            name : "CSK",
-                            Players :[ "MSD","Raina"],
+                            name : "Chennai Super Kings",
+                            image : "https://pabitraeditography.com/wp-content/uploads/2023/03/full-hd-csk-wallpaper8-min-537x1024.jpg",
                             Staff :["mike hussey","steven fleming"],
                             totalscore:1000,
-                            image : "https://pabitraeditography.com/wp-content/uploads/2023/03/full-hd-csk-wallpaper8-min-537x1024.jpg"
+                            Team :{
+                                Batsmen : ["Ruturaj (C)","Rachin","Dary Michell","Shivam Dube","Rahane","Dhoni (wk)"],
+                                Bowler  : ["Jadeja","Chahar","Pathernna","Rahaman","S Thakur"]
+                            }
                         }
                         ,
                         {
-                            name : "RCB",
-                            Players :[ "VK","DK"],
+                            name : "Royal Changellers Bangalore",
+                            image :"https://w0.peakpx.com/wallpaper/1021/408/HD-wallpaper-virat-rcb-virat-kholi-thumbnail.jpg",
                             Staff :["Hesson","Don"],
                             totalscore:500,
-                            image :"https://w0.peakpx.com/wallpaper/1021/408/HD-wallpaper-virat-rcb-virat-kholi-thumbnail.jpg"
+                            Team :{
+                                Batsmen : ["Virat","Faf (C)","Green","Maxwell","Anjum rawat","DK (wk)"],
+                                Bowler  : ["Yash","topely","siraj","karn","ferguson"]
+                            }
                         }
                         ,
                         {
-                            name : "MI",
-                            Players :[ "ROHIT","SKY"],
+                            name : "Mumbai Indians",
+                            image :"https://pbs.twimg.com/media/C-fwdyLXoAACFFz.jpg",
                             Staff :["Sachin","Malinga"],
                             totalscore:600,
-                            image :"https://pbs.twimg.com/media/C-fwdyLXoAACFFz.jpg"
-                        },
+                            Team :{
+                                Batsmen : ["Rohit","Ishan (wk)","Sky","Tilak","Tim David","Shepherd","Hardik (C)"],
+                                Bowler  : ["Chawala","Jasprit","Gozeez","Akash"]
+                            }
+                        }
+                        ,
                         {
-                            name : "KKR",
-                            Players :[ "NARINe","SALT"],
-                            Staff :["GG","BA"],
+                            name : "Kolkata Knight Riders",
+                            image :"https://i.pinimg.com/originals/16/86/48/168648339bbecb1ad59de36f59c79c9e.jpg",
+                            Staff :["Gautam Gambhir","Bharth Arun"],
                             totalscore:600,
-                            image :"https://i.pinimg.com/originals/16/86/48/168648339bbecb1ad59de36f59c79c9e.jpg"
+                            Team :{
+                                Batsmen : ["Salt (wk)","Narine","Anush","Iyer (C)","Rinku","Ramandeep","Venky Iyer"],
+                                Bowler  : ["Russel","Harshit","Varun","Suyas"]
+                            }
+                        }
+                        ,
+                        {
+                            name : "Sunrisers Hyderabad",
+                            image :"https://upload.wikimedia.org/wikipedia/en/e/eb/Sunrisers_Hyderabad.png",
+                            Staff :["Vettori","VVS"],
+                            totalscore:600,
+                            Team :{
+                                Batsmen : ["Head","Abhiseik","Adein","H Kalseen (wk)","Samad","Shazed","Nitish Kumar Reddy"],
+                                Bowler  : ["Pat Cummins (C)","Bhuvi","Natarajan","Mayank"]
+                            }
+                        }
+                        ,
+                        {
+                            name : "Rajasthan Royals",
+                            image :"https://upload.wikimedia.org/wikipedia/en/thumb/5/5c/This_is_the_logo_for_Rajasthan_Royals%2C_a_cricket_team_playing_in_the_Indian_Premier_League_%28IPL%29.svg/1200px-This_is_the_logo_for_Rajasthan_Royals%2C_a_cricket_team_playing_in_the_Indian_Premier_League_%28IPL%29.svg.png",
+                            Staff :["Sangakara","Dravid"],
+                            totalscore:600,
+                            Team :{
+                                Batsmen : ["Butler","Jaswal","Samson (C)(wk)","Riyan","Jurel","Hetmyer","Powell"],
+                                Bowler  : ["Trent Boult","Ashwin","Chahal","Sandeep Sharma"]
+                            }
                         }
             ]
+
+            const winningcount = () => {
+                const totalScore = IPLHistory.reduce((acc, team) => acc + team.totalscore, 0);
+                return totalScore;
+            };
+           
         
     
-    return (
-        
-        <React.Fragment>  
-           {
-                IPLHistory.map((value,index)=>
-                {
-                    const {name,Players,Staff,totalscore,image}=value
-                    return(
+            return (
                 <>
-                { value.totalscore>0 &&
-                        <>
-                        <h2>{index+1} .{name}</h2>
-                        {
-                            Players.map(player=>
-                            <>
-                            <h4>{player}</h4>
-                            </>
-                            )
-                        }
-                        {
-                            Staff.map(staff=>
+                    {IPLHistory.filter(eachTeam => eachTeam.totalscore >= 100).map((value, index) => {
+                        const { name, Staff, totalscore, image, Team } = value;
+                        return (
+                            value.totalscore > 0 && (
                                 <>
-                                <h4>{staff}</h4>
+                                    <h1>{index + 1}. {name}</h1>
+                                    <img src={image} height={200} width={150} alt="imageofcap" />
+        
+                                    <h3>Staff:</h3>
+                                    <ul>
+                                        {Staff.map((staff, index) => (
+                                            <li key={index}>{staff}</li>
+                                        ))}
+                                    </ul>
+        
+                                    <h3>Batsmen:</h3>
+                                    <ul>
+                                        {Team.Batsmen.map((batsman, index) => (
+                                            <li key={index}>{batsman}</li>
+                                        ))}
+                                    </ul>
+        
+                                    <h3>Bowlers:</h3>
+                                    <ul>
+                                        {Team.Bowler.map((bowler, index) => (
+                                            <li key={index}>{bowler}</li>
+                                        ))}
+                                    </ul>
+        
+                                    <h4>Total Score is : {totalscore}</h4><br></br>
                                 </>
                             )
-                        }
-
-                        {
-                                <>
-                                <h4>{totalscore}</h4>
-                                </>
-                        }
-                        {
-                                <>
-                                <img src={image} height={150} width={100} alt ="imageofcap"></img>
-                                </>
-
-                        }
-
-
-                        </>
-               
-                    }
+                        );
+                    })}
+                    <h4>Total winning score: {winningcount()}</h4>
                 </>
-                    )
-                }
-            )
-           }
-        </React.Fragment>
-
-)
-
-}
-
-export default NestedLists
+            );
+        };
+        
+        export default NestedLists;
 
 // const IPLMeans = ["INDIAN PERMIER LEAGUE"]
 //     const IPLIMAGE = "https://www.hindustantimes.com/ht-img/img/2023/12/20/1600x900/Ipl-auction_1703034762917_1703034773943.jpg"
